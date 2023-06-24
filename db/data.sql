095031c05e4dff2b41d472d97464d9ff75d7c3f2
MOOK <mukmookk@gmail.com>
2023-06-24 16:42:39 +0900

    Add sample data for train

---
+++ db/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO stocks (company_name, stock_price) VALUES ("Microsoft", 280.0);
+INSERT INTO stocks (company_name, stock_price) VALUES ("Amazon", 3300.0);
+INSERT INTO stocks (company_name, stock_price) VALUES ("Google", 2400.0);
+INSERT INTO stocks (company_name, stock_price) VALUES ("Tesla", 600.0);
+INSERT INTO stocks (company_name, stock_price) VALUES ("Facebook", 350.0);
+INSERT INTO stocks (company_name, stock_price) VALUES ("Twitter", 60.0);
+INSERT INTO stocks (company_name, stock_price) VALUES ("Netflix", 500.0);
+INSERT INTO stocks (company_name, stock_price) VALUES ("NVIDIA", 750.0);
+INSERT INTO stocks (company_name, stock_price) VALUES ("Adobe", 550.0);
+INSERT INTO stocks (company_name, stock_price) VALUES ("Oracle", 80.0);
